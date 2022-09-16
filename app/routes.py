5dac2db7412180d77c94ee19a3b0397e4a64286d
demo_project <angkushtarachand@gmail.com>
2022-09-16 20:27:00 +0400

    commenting new

--- app/routes.py
+++ app/routes.py
@@ -6,18 +6,18 @@ import flask
 def index():
     return flask.render_template('index.html')
 
-
-@flask_app.route("/register", method=("GET", "POST"))
-def register():
-    my_register_form = Form()
-    if my_register_form.validate_on_submit():
-
-        t_username = my_register_form.username.data
-        t_password = my_register_form.password.data
-        t_email = my_register_form.email.data
-
-    else:
-
-        return "Something went wrong", flask.redirect("/")
-
-    return flask.render_template("profile.html", form=my_register_form)
+#
+# @flask_app.route("/register", method=("GET", "POST"))
+# def register():
+#     my_register_form = Form()
+#     if my_register_form.validate_on_submit():
+#
+#         t_username = my_register_form.username.data
+#         t_password = my_register_form.password.data
+#         t_email = my_register_form.email.data
+#
+#     else:
+#
+#         return "Something went wrong", flask.redirect("/")
+#
+#     return flask.render_template("profile.html", form=my_register_form)
