168a8582675c8625f6edc2384a8eeb207a783e26
demo_project <angkushtarachand@gmail.com>
2022-09-16 20:20:42 +0400

    testing

--- app/routes.py
+++ app/routes.py
@@ -7,11 +7,17 @@ def index():
     return flask.render_template('index.html')
 
 
-@flask_app.route("/register")
+@flask_app.route("/register", method=("GET", "POST"))
 def register():
     my_register_form = Form()
     if my_register_form.validate_on_submit():
 
         t_username = my_register_form.username.data
         t_password = my_register_form.password.data
-        t_email = my_register_form.
+        t_email = my_register_form.email.data
+
+    else:
+
+        return "Something went wrong", flask.redirect("/")
+
+    return flask.render_template("profile.html", form=my_register_form)
